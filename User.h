3d54d922e060d0abd8117388fc1c0e9a26cc0e0b
Konrad Rutkowski <konrad.rutkowski.programista@gmail.com>
2022-11-24 08:10:29 +0100

    Added User Class

---
+++ User.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+using namespace std;
+
+class User {
+
+    int id;
+    string firstName;
+    string lastName;
+    string login;
+    string password;
+
+    public:
+    void setId(int newId);
+    void setFirstName(string newFirstName);
+    void setLastName(string newLastName);
+    void setLogin(string newLogin);
+    void setPassword(string newPassword);
+
+    int getId();
+    string getFirstName();
+    string getLastName();
+    string getLogin();
+    string getPassword();
+
+};
