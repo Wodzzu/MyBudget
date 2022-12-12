e6ae24bed5a12d39fcce72d6a6a3fae1f8b96ea7
Konrad Rutkowski <konrad.rutkowski.programista@gmail.com>
2022-12-12 10:54:42 +0100

    Added BudgetManager Class

---
+++ BudgetManager.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "UserManager.h"
+#include "IncomeFile.h"
+
+using namespace std;
+
+class MyBudget {
+
+const string INCOME_FILE_NAME;
+const string EXPENSE_FILE_NAME;
+
+
+public:
+
+};
